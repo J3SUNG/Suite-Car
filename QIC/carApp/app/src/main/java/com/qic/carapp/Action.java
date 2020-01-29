a8ab8a4b429ca978b00c1170d54a607e512a0cba
syg4806 <syg4806@gmail.com>
2020-01-29 10:21:36 -0800

    init

---
+++ QIC/carApp/app/src/main/java/com/qic/carapp/Action.java
@@ -0,0 +1,13 @@
+package com.qic.carapp;
+
+public enum Action {
+    WO,WC,AIC,AIN,AIH,AOC,AON,AOH
+    /*WINDOW_OPEN
+    WINDOW_CLOSE
+    AIR_IN_COLD
+    AIR_IN_NORMAL
+    AIR_IN_HOT
+    AIR_OUT_COLD
+    AIR_OUT_NORMAL
+    AIR_OUT_HOT */
+}
