2e3211fa2314cf11e5119bab25a63b6e4a6c3394
박승수 <pss4190@naver.com>
2020-02-11 14:57:31 -0800

    Home & User management modified
    update sendmail & signup with ajax

--- Server/iot/your-app/app/src/controllers/HomeController.php
+++ Server/iot/your-app/app/src/controllers/HomeController.php
@@ -19,65 +19,6 @@ final class HomeController extends BaseController
     {
     	$this->view->render($response, 'login.twig');
 	}
-<<<<<<< Updated upstream
-	//Request $request, Response $response, $args
-	//e-mail sender : under the check of all input
-    public function sendMail($email, $username, $auth_code, $temp)
-	{
-		//data for email phpmailer
-		if($temp == SIGN_UP)
-		{
-			$message = "<a href=http://teamc-iot.calit2.net/signup_authorization?link=$auth_code&user=$username&temp=$temp>
-						<img src='cid:Logoimage'></a><h1>THANK YOU</h1><h2>Please Click the image above to activate your account.</h2>
-						<br>";
-		}
-		else if($temp == FORGOTTEN)
-		{
-			$message = "<img src='cid:Logoimage'><h1>Don't Worry</h1><h2>Your New Temporary Password is Issued Below</h2>
-						<br>$auth_code<br>";
-		}
-					
-		//email send (problem occur : maybe composer process need)
-		$mail = new PHPMailer(true);
-
-		try {
-			//Server settings
-			//$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-			/*$mail->isSMTP();                                            // Send using SMTP
-			$mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-			$mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-			$mail->Username   = 'wptjd6141@gamil.com';                     // SMTP username
-			$mail->Password   = 'wpgns213';                               // SMTP password
-			$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-			$mail->Port       = 587;                                    // TCP port to connect to
-
-			//Recipients
-			$mail->setFrom($email, $username);
-			$mail->addAddress($email);               // Name is optional
-			//$mail->addReplyTo('info@example.com', 'Information');
-			//$mail->addCC('cc@example.com');
-			//$mail->addBCC('bcc@example.com');
-			//Attachments
-			//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-			//$mail->addAttachment('images/icon/logo.png', 'new.jpg');    // Optional name  'images/icon/logo.png', 'testImage', 'images/icon/logo.png'
-			//Content
-			$mail->isHTML(true);                                  // Set email format to HTML
-			$mail->Subject = "SuiteCar Issued E-mail to $username";
-			$mail->AddEmbeddedImage('images/logo.png', 'Logoimage');
-			$mail->Body=$message;
-			$mail->AltBody = 'Thank you . Please Click the link to activate your account.';
-			$mail->send();*/
-			echo "<script>alert(\"E-mail has been sent. Check your E-mail.\");</script>";
-			echo "<script>location.replace('login')</script>";
-
-			echo "not server setting<br>";
-		}
-		catch (Exception $e) {
-			echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-			//$this->view->render($response, 'failure_page.twig', ['alert_message' => "Message could not be sent. Mailer Error: {$mail->ErrorInfo}"]);
-	}
-=======
->>>>>>> Stashed changes
 
     public function signup(Request $request, Response $response, $args)
     {
