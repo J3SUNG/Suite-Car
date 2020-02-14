bcefcf319474cbd4441cfd517089e434a18c2eb8
박승수 <pss4190@naver.com>
2020-02-14 06:04:31 -0800

    Google map made
    map circle show up and info window modified

--- Server/iot/your-app/app/src/controllers/DataManagement.php
+++ Server/iot/your-app/app/src/controllers/DataManagement.php
@@ -13,10 +13,13 @@ final class DataManagement extends BaseController
 {
 	public function db_data_for_map(Request $request, Response $response, $args)
 	{
-		$sql = "SELECT Users.username as username, Air_data.*
-		FROM Users, Air_data
-		INNER JOIN Sensors ON Air_data.sensor_no = Sensors.sensor_no
-		INNER JOIN Users U2 ON Sensors.user_no = U2.user_no;";
+		$sql = "SELECT Users.username, a.*
+				FROM Users, Air_data AS a
+				JOIN(
+					SELECT sensor_no, MAX(time_in) time_in 
+					FROM Air_data
+					GROUP BY sensor_no) AS b
+				ON a.sensor_no = b.sensor_no AND a.time_in = b.time_in;";
 		$stmt= $this->em->getConnection()->prepare($sql);
 		$stmt->execute();
 		$result = $stmt->fetchAll();
@@ -29,7 +32,7 @@ final class DataManagement extends BaseController
 				array("username"=>$map_data['username'],
 				"Air_data_no"=>$map_data['Air_data_no'],
 				"sensor_no"=>$map_data['sensor_no'],
-				"time"=>$map_data['time'],
+				"time"=>$map_data['time_in'],
 				"CO_raw"=>$map_data['CO_raw'],
 				"CO_aqi"=>$map_data['CO_aqi'],
 				"SO2_raw"=>$map_data['SO2_raw'],
@@ -38,8 +41,8 @@ final class DataManagement extends BaseController
 				"NO2_aqi"=>$map_data['NO2_aqi'],
 				"O3_raw"=>$map_data['O3_raw'],
 				"O3_aqi"=>$map_data['O3_aqi'],
-				"PM2.5_raw"=>$map_data['PM2.5_raw'],
-				"PM2.5_aqi"=>$map_data['PM2.5_aqi'],
+				"PM25_raw"=>$map_data['PM2.5_raw'],
+				"PM25_aqi"=>$map_data['PM2.5_aqi'],
 				"center"=>array("lat"=>$map_data['latitude'],
 								"lng"=>$map_data['longtitude'])
 			);
